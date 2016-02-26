067224049b2fbc4239e50db7b062529448650ab7
Bliznyuk Sergey <bliznyuk.sergey@gmail.com>
2016-02-26 16:45:37 +0200

    Test git push all. WorkToHome

--- src/startRobot/Robot.java
+++ src/startRobot/Robot.java
@@ -3,10 +3,22 @@ package startRobot;
 public class Robot {
 
     // Текущая координата X
-    int x = 0;
+    double x = 0;
     // Текущая координата Y
-    int y = 0;
+    double y = 0;
     // Текущий курс (в градусах)
-    int course = 0;
+    double course = 0;
+
+    void printCoordinates() {
+        System.out.println(x + "," + y);
+    }
+
+    // Передвижение на дистанцию distance
+    void forward(int distance) {
+        // Обращение к полю объекта X
+        x = x + distance * Math.cos(course / 180 * Math.PI);
+        // Обращение к полю объекта Y
+        y = y + distance * Math.sin(course / 180 * Math.PI);
+    }
 
 }
--- src/startRobot/RobotManager.java
+++ src/startRobot/RobotManager.java
@@ -4,11 +4,38 @@ public class RobotManager {
 
     public static void main(String[] args) {
 
-        Robot r1 = new Robot();
+         /*Robot r1 = new Robot();
         Robot r2;
         r2 = new Robot();
 
         r1.x = 99;
+        r2.x = 123;
+
+        System.out.println(r1.x);
+        System.out.println(r2.x);*/
+
+        // Создаем объекта класса Robot
+        Robot robot = new Robot();
+
+        // Вперед на 20 метров
+        robot.forward(20);
+        // Напечатать координаты
+        robot.printCoordinates();
+
+        // Курс 90 градусов - не самый правильный способ
+        // но давайте пока остановимся на нем
+        robot.course = 90;
+        // Вперед на 20 метров
+        robot.forward(20);
+        // Напечатать координаты
+        robot.printCoordinates();
+
+        // Курс 45 градусов
+        robot.course = 45;
+        // Вперед на 20 метров
+        robot.forward(20);
+        // Напечатать координаты
+        robot.printCoordinates();
 
     }
 }
