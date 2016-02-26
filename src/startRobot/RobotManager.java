ee572cda45fb733a72d18ec6c4f6fffe0dba3803
Bliznyuk Sergey <bliznyuk.sergey@gmail.com>
2016-02-26 13:09:44 +0200

    Test git push all. WorkToHome

--- src/startRobot/RobotManager.java
+++ src/startRobot/RobotManager.java
@@ -4,8 +4,11 @@ public class RobotManager {
 
     public static void main(String[] args) {
 
-        Robot r1;
+        Robot r1 = new Robot();
         Robot r2;
+        r2 = new Robot();
+
+        r1.x = 99;
 
     }
 }
