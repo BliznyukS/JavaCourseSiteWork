05c73a621a620923578cb1cef21f788a1a1b20cd
BliznyukS <bliznyuk.sergey@gmail.com>
2016-02-27 15:18:10 +0200

    Robot 01

--- src/secondRobot/Robot01.java
+++ src/secondRobot/Robot01.java
@@ -1,4 +1,15 @@
 package secondRobot;
 
 public class Robot01 {
+
+    // Текущий курс (в градусах)
+    private double course = 0;
+
+    public double getCourse() {
+        return course;
+    }
+
+    public void setCourse(double course) {
+        this.course = course;
+    }
 }
--- src/secondRobot/RobotManager01.java
+++ src/secondRobot/RobotManager01.java
@@ -1,4 +1,26 @@
 package secondRobot;
 
 public class RobotManager01 {
+
+    public static void main(String[] args) {
+        // Создаем объект для управления роботом
+        RobotManager01 rm = new RobotManager01();
+
+        // Создаем объекта класса Robot
+        Robot01 robot = new Robot01();
+        // Курс 45 градусов
+        robot.setCourse(45);
+        // Напечатать курс
+        System.out.println(robot.getCourse());      // Здесь будет 45
+
+        // вызываем метод и передаем туда робота
+        rm.changeCourse(robot);
+
+        // Напечатать курс
+        System.out.println(robot.getCourse());      // Здесь будет 180
+    }
+
+    private void changeCourse(Robot01 robot) {
+        robot.setCourse(180);
+    }
 }
