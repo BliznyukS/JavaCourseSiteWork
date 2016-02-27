c9805736e79c016aa3bcd68f17a5643935e1875a
Bliznyuk Sergey <bliznyuk.sergey@gmail.com>
2016-02-27 20:56:41 +0200

    Override Robot

---
+++ src/fourthRobot/Robot04.java
@@ -0,0 +1,15 @@
+package fourthRobot;
+
+public class Robot04 {
+
+    private String name;
+
+    public Robot04(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "name=" + name;
+    }
+}
---
+++ src/fourthRobot/RobotManager04.java
@@ -0,0 +1,11 @@
+package fourthRobot;
+
+public class RobotManager04 {
+
+    public static void main(String[] args) {
+        Robot04 robot1 = new Robot04("Test Robot 1");
+        Robot04 robot2 = new Robot04("Test Robot 2");
+        System.out.println(robot1.toString());
+        System.out.println(robot2.toString());
+    }
+}
