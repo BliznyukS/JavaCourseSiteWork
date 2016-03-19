8a2520d0985846b6d098cb2a94259f692e8b426a
BliznyukS <bliznyuk.sergey@gmail.com>
2016-03-19 14:28:44 +0200

    Robot 03 drawing

--- src/FinalClassTest.java
+++ src/FinalClassTest.java
@@ -1,15 +1,15 @@
 public class FinalClassTest {
 
-    /*public static void main(String[] arg) {
+    public static void main(String[] arg) {
         final Integer f = new Integer(100);
         f = 200;
-    }*/
+    }
 
-    private final Integer value;
+   /* private final Integer value;
 
     public FinalClassTest() {
         // Здесь будет ошибка, если инициализация
         // будет сделана в описании свойства
         value = 100;
-    }
+    }*/
 }
