3e7caddc5faf8225661bcec25260b53c04d1ba83
BliznyukS <bliznyuk.sergey@gmail.com>
2016-03-19 14:28:17 +0200

    Robot 03 drawing

---
+++ src/FinalClassTest.java
@@ -0,0 +1,15 @@
+public class FinalClassTest {
+
+    /*public static void main(String[] arg) {
+        final Integer f = new Integer(100);
+        f = 200;
+    }*/
+
+    private final Integer value;
+
+    public FinalClassTest() {
+        // Здесь будет ошибка, если инициализация
+        // будет сделана в описании свойства
+        value = 100;
+    }
+}
---
+++ src/StaticTestClass.java
@@ -0,0 +1,9 @@
+
+public class StaticTestClass {
+    private static Integer staticValue = 99;
+    private Integer simpleValue = 99;
+
+    public static void main(String[] arg) {
+        System.out.println(StaticTestClass.staticValue);
+    }
+}
